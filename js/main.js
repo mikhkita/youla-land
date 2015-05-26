f7ff1a26c8b845b970796563de3954ee3693503c
Михаил <beatbox787@gmail.com>
2015-05-26 13:59:49 +0600

    no message

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){	
+    var brow = browser(),
+        tmp = brow.splice(","),
+    
+    brow = (brow == "MSIE,8.0")?"MSIE":tmp[0];
+    function browser(){
+        var N= navigator.appName, ua= navigator.userAgent, tem;
+        var M= ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+        if(M && (tem= ua.match(/version\/([\.\d]+)/i))!= null) M[2]= tem[1];
+        M= M? [M[1], M[2]]: [N, navigator.appVersion,'-?'];
+        return M;
+    }
+
     function resize(){
        if( typeof( window.innerWidth ) == 'number' ) {
             myWidth = window.innerWidth;
@@ -11,9 +23,42 @@ $(document).ready(function(){
             myWidth = document.body.clientWidth;
             myHeight = document.body.clientHeight;
         }
+
+        $(".b-3 .b-back").css("height",( myHeight > myWidth )?1000:750);
     }
     $(window).resize(resize);
     resize();
+
+    $(window).stellar({
+        hideDistantElements: false,
+        horizontalScrolling: false,
+        positionProperty: 'transform'
+    });
+
+    $(".b-gradient-butt").mousemove(function(e){
+        var mX = e.clientX-$(this).offset().left,
+            w = 300,
+            p = Math.round((80*(mX/w))+10);
+
+        $(this).find(".b-center-point").attr("offset",p+"%");
+    }).mousedown(function(){
+        $(this).find(".b-gradient-rect").attr("fill",(brow == "Safari")?("rgba(42, 48, 255, 0.1)"):("url(#"+$(this).attr("data-id")+"2)"));
+    }).mouseup(function(){
+        $(this).find(".b-gradient-rect").attr("fill",(brow == "Safari")?("rgba(42, 48, 255, 0.05)"):("url(#"+$(this).attr("data-id")+"1)"));
+    }).hover(function(){
+        $(this).find(".b-gradient-rect").attr("fill",(brow == "Safari")?("rgba(42, 48, 255, 0.05)"):("url(#"+$(this).attr("data-id")+"1)"));
+    },function(){
+        $(this).find(".b-gradient-rect").attr("fill","none");
+        $(this).find(".b-gradient-text").attr("fill","url(#"+$(this).attr("data-id")+")");
+    });
+    $("body").on("dragleave",resetActive);
+
+    function resetActive(){
+        $(".b-gradient-rect").attr("fill","none");
+        $(".b-gradient-butt").each(function(){
+            $(this).find(".b-gradient-text").attr("fill","url(#"+$(this).attr("data-id")+")");
+        });
+    }
     
 	// var myPlace = new google.maps.LatLng(55.754407, 37.625151);
  //    var myOptions = {
